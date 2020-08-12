78101898d5970ade81fdf8a6c99418bc1ce9e812
jdavis3333 <jdavis3333@gmail.com>
2020-08-11 21:44:54 -0700

    Updates

---
+++ precache-manifest.7e04467963cc3ebf2d32db286faf5512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bd3a183a86c70c1b307ecc2f3cefeb",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "1cc92c37a81839e1c0a9",
+    "url": "/clicky_game/static/css/main.abaa78ca.chunk.css"
+  },
+  {
+    "revision": "9850722b3c8068f369fe",
+    "url": "/clicky_game/static/js/2.75350cfc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clicky_game/static/js/2.75350cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc92c37a81839e1c0a9",
+    "url": "/clicky_game/static/js/main.dc3ece23.chunk.js"
+  },
+  {
+    "revision": "5b86ab7a3c3994df01bd",
+    "url": "/clicky_game/static/js/runtime-main.237441d5.js"
+  }
+]);
